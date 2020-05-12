731a081679641ffcfd9811a3d2077a83efc51638
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-05-12 18:57:28 -0500

    Tested Atom Beautify package.

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -5,63 +5,63 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Customer;
 
-
 class CustomersController extends Controller
 {
-  public function index()
-  {
-    // dd('hello');
-    $customers = Customer::all();
+    public function index()
+    {
+        // dd('hello');
+        $customers = Customer::all();
 
-     return view ('customers.index', ['customers' => $customers]);
-  }
+        return view('customers.index', ['customers' => $customers]);
+    }
 
-  public function create()
-  {
+    public function create()
+    {
 
     //dd('hello');
-    return view ('customers.create');
-  }
+        return view('customers.create');
+    }
 
-  public function store()
-  {
-    // dd('inside store');
+    public function store()
+    {
+        // dd('inside store');
 
-    Customer::create(request(['first_name','last_name']));
-    return redirect('/customers');
-  }
+        Customer::create(request(['first_name','last_name']));
+        return redirect('/customers');
+    }
 
-  public function show($id) {
-      //dd('hello world');
+    public function show($id)
+    {
+        //dd('hello world');
         $customers = Customer::findOrFail($id);
-        return view ('customers.show',compact('customer'));
- }
+        return view('customers.show', compact('customer'));
+    }
 
- public function edit(Customer $customer)
- {
-   // $customer = Customer::find($id);
-   return view('customers.edit', compact('customer'));
- }
+    public function edit(Customer $customer)
+    {
+        // $customer = Customer::find($id);
+        return view('customers.edit', compact('customer'));
+    }
 
- public function update(Customer $customer)
- {
-   // $customer = Customer::find($id);
-   // dd('inside update');
-   $customer->first_name = request('first_name');
-   $customer->last_name = request('last_name');
+    public function update(Customer $customer)
+    {
+        // $customer = Customer::find($id);
+        // dd('inside update');
+        $customer->first_name = request('first_name');
+        $customer->last_name = request('last_name');
 
-   $customer->save();
+        $customer->save();
 
-   return redirect('/customers');
- }
+        return redirect('/customers');
+    }
 
- public function destroy($id)
- {
-   $customer = Customer::find($id);
-   //dd('hello');
-   //$customers->delete($id);
-   customer::find($id)->delete();
-/// issue is possible here////
-   return redirect('/customers');
- }
+    public function destroy($id)
+    {
+        $customer = Customer::find($id);
+        //dd('hello');
+        //$customers->delete($id);
+        customer::find($id)->delete();
+        /// issue is possible here////
+        return redirect('/customers');
+    }
 }

24fbea502c57fe02ccf522dae95607495c962e6f
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-05-13 00:30:23 -0500

    Added customers table partial to customers index view.

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -25,31 +25,7 @@
     <th></th>
     <th></th>
   </tr>
-    @foreach ($customers as $customer)
-      <tr>
-        <td>{{ $customer->first_name }}</td>
-        <td>{{ $customer->last_name }}</td>
-        <td>
-          <a href="/customers/{{ $customer->id }}/edit">
-            <button type="submit">Edit</button>
-          </a>
-        </td>
-        <td>
-          <form method="POST" action="/customers/{{ $customer->id }}">
-            @method('DELETE')
-            @csrf
-            <p>
-              <button type="submit">Delete</button>
-            </p>
-          </form>
-        </td>
-        <td>
-          <a href="/reservations/create/{{ $customer->id }}">
-            <button type="submit">Create Reservation</button>
-          </a>
-        </td>
-      </tr>
-    @endforeach
+    @include('customers.customers-partial')
     </table>
   </body>
 </html>

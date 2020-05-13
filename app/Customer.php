cb0055d7a9873387f1482314c73325d84b6601f4
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-05-12 19:01:03 -0500

    Tested beautify and html validation package.

--- app/Customer.php
+++ app/Customer.php
@@ -4,18 +4,11 @@
 
 namespace App;
 
-
-
 use Illuminate\Database\Eloquent\Model;
 
-
-
 class Customer extends Model
-
 {
-
     protected $fillable = [
       'first_name' , 'last_name'
     ];
-
 }
--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -4,35 +4,36 @@
 
 <head>
 
-  <meta charset="UTF-8">
+    <meta charset="UTF-8">
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <title>Create Customer</title>
+    <title>Create Customer</title>
 
 </head>
+
 <body>
-  <p>
-    <a href="/">Home</a>
-  </p>
+    <p>
+        <a href="/">Home</a>
+    </p>
 
-  <h1>Create Customer</h1>
+    <h1>Create Customer</h1>
 
-  <form method="POST" action="/customers">
-    @csrf
+    <form method="POST" action="/customers">
+        @csrf
 
-    <div>
-  		<label for="first_name">First Name:</label>
-  		<input class="input" type="text" id="first_name" name="first_name" value="" /><br />
+        <div>
+            <label for="first_name">First Name:</label>
+            <input class="input" type="text" id="first_name" name="first_name" value="" /><br />
 
-  		<label for="last_name">Last Name:</label>
-  		<input class="input" type="text" id="last_name" name="last_name" value="" /><br />
+            <label for="last_name">Last Name:</label>
+            <input class="input" type="text" id="last_name" name="last_name" value="" /><br />
 
-  		<button type="submit">Save</button>
-    </div>
-  </form>
+            <button type="submit">Save</button>
+        </div>
+    </form>
 
 </body>
 
